37289f5c20d625d820705445dc90ae0063dc874b
EgorSivenko <egor.siv20@gmail.com>
2024-05-26 15:37:08 +0300

    Added custom secret key for JSON Web Token

--- src/main/java/org/example/planner/security/jwt/JwtProvider.java
+++ src/main/java/org/example/planner/security/jwt/JwtProvider.java
@@ -1,30 +1,42 @@
 package org.example.planner.security.jwt;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
 public class JwtProvider {
 
     private static final int EXPIRATION_TIME = 30 * 60 * 1000;
-    private static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
+
+    @Value("${jwt.secret-key}")
+    private String key;
+    private SecretKey secretKey;
+
+    @PostConstruct
+    public void init() {
+        secretKey = Keys.hmacShaKeyFor(key.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String generateToken(Authentication authentication) {
         return Jwts.builder()
                 .subject(authentication.getName())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SECRET_KEY)
+                .signWith(secretKey)
                 .compact();
     }
 
     public String getEmailFromToken(String token) {
         return Jwts.parser()
-                .verifyWith(SECRET_KEY)
+                .verifyWith(secretKey)
                 .build()
                 .parseSignedClaims(token)
                 .getPayload()
